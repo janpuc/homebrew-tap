034e4edddf0e0d15a2dce415bb4f85ec7359d4f4
Jan Pucilowski <jan.pucilowski99@gmail.com>
2024-02-08 13:29:13 +0100

    feat: add sha256 to pongo tap

--- Formula/pongo.rb
+++ Formula/pongo.rb
@@ -5,7 +5,7 @@ class Pongo < Formula
   license "Apache-2.0"
 
   url "https://github.com/Kong/kong-pongo/archive/refs/tags/2.9.0.tar.gz"
-  sha256 ""
+  sha256 "51e94e0e5c38fc10ce8db40055f43511b88abdb8042d9949f414049cd94fbbdb"
 
   def install
     bin.install "pongo.sh" => "pongo"
